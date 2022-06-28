3bf9d097e860324142eeeca223246007ba17f69f
Sandip <sandipgaikwad05@gmail.com>
2022-06-28 08:23:16 +0100

    WIP: Short URL project - Added model - Updated settings - Added initial migration - Database added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class short_url(models.Model):
+    new = models.CharField(max_length=200)
+    old = models.TextField()
+    slug = models.CharField(max_length=200)
+    time = models.DateTimeField(auto_now_add=True)
